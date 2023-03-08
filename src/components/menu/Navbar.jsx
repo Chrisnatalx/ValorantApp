34cb573aadcb4f3ac29f5c00aa686db2f4c0e77d
Chrisnatalx <natale.christian.a@gmail.com>
2023-03-08 14:34:02 -0300

    Valorant logo added

--- src/components/menu/Navbar.jsx
+++ src/components/menu/Navbar.jsx
@@ -1,22 +1,29 @@
 import React from "react";
 import { Link as RouterLink } from "react-router-dom";
 import { Logo } from "../logo/Logo";
+import { RiotLogo } from "../logo/RiotLogo";
 import "./Navbar.css";
 import {
 	Box,
 	Button,
 	ButtonGroup,
 	Flex,
+	IconButton,
 	Link,
 	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
 } from "@chakra-ui/react";
 import { TbWorld } from 'react-icons/tb'
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 export const Navbar = () => {
 	return (
 		<>
 			<Box
 				fontFamily="FF Mark W05,Arial,sans-serif"
+				height='80px'
 				position="fixed"
 				zIndex="1001"
 				w="100%"
@@ -29,10 +36,14 @@ export const Navbar = () => {
 					gap="2"
 				>
 					<Menu>
-						<ButtonGroup display="flex" justifyContent="space-around">
+						<ButtonGroup display="flex" justifyContent="space-around" gap={8}>
+							<Link as={RouterLink} to="/" style={{ textDecoration: "none" }}>
+								<RiotLogo className="LogoRiot" />
+							</Link>
 							<Link as={RouterLink} to="/" style={{ textDecoration: "none" }}>
 								<Logo className="Logo" />
 							</Link>
+
 						</ButtonGroup>
 						<ButtonGroup display="flex" justifyContent="space-around" gap={8}>
 							<Link
@@ -60,7 +71,7 @@ export const Navbar = () => {
 									variant="link"
 									color="#f9f9f9"
 									size="sm"
-									_hover={{ textDecoration: "underline solid #FF5A5A 2px" }}
+									_hover={{ textDecoration: "underline solid #ff4655 2px" }}
 								>
 									MAPS
 								</Button>
@@ -75,21 +86,61 @@ export const Navbar = () => {
 									variant="link"
 									color="#f9f9f9"
 									size="sm"
-									_hover={{ textDecoration: "underline solid #FF5A5A 2px" }}
+									_hover={{ textDecoration: "underline solid #ff4655 2px" }}
 								>
 									WEAPONS
 								</Button>
 							</Link>
+							<MenuButton as={Button} variant="link" className="MenuButton"
+								_hover={{ textDecoration: "underline solid #ff4655 2px" }}
+								_expanded={{ textDecoration: "underline solid #ff4655 2px", transform: 'scale(1.25,1.25)' }}
+								color="#f9f9f9"
+								size="sm" rightIcon={<ChevronDownIcon />}
+								_active={{ color: '#f9f9f9' }}>
+								OUR SOCIALS
+							</MenuButton>
+							<MenuList bg='#1f1f1f' borderColor='#1f1f1f' >
+								<Link as={RouterLink} to='https://twitter.com/playvalorant' style={{ textDecoration: 'none' }}>
+									<MenuItem bg='#1f1f1f' color='#DBD9D9' _hover={{ bg: '#424242', color: '#f2f2f2' }}>
+										Twitter
+									</MenuItem>
+								</Link>
+								<Link as={RouterLink} to='https://www.instagram.com/playvalorantofficial/' style={{ textDecoration: 'none' }}
+								>
+									<MenuItem bg='#1f1f1f' color='#DBD9D9' _hover={{ bg: '#424242', color: '#f2f2f2' }}>
+										Instagram
+									</MenuItem>
+								</Link>
+								<Link as={RouterLink} to='https://www.facebook.com/PlayVALORANT/' style={{ textDecoration: 'none' }}
+								>
+									<MenuItem bg='#1f1f1f' color='#DBD9D9' _hover={{ bg: '#424242', color: '#f2f2f2' }}>
+										Facebook
+									</MenuItem>
+								</Link>
+								<Link as={RouterLink} to='https://www.youtube.com/PlayValorant' style={{ textDecoration: 'none' }}
+								>
+									<MenuItem bg='#1f1f1f' color='#DBD9D9' _hover={{ bg: '#424242', color: '#f2f2f2' }}>
+										Youtube
+									</MenuItem>
+								</Link>
+								<Link as={RouterLink} to='https://discord.com/invite/valorant' style={{ textDecoration: 'none' }}>
+									<MenuItem bg='#1f1f1f' color='#DBD9D9' _hover={{ bg: '#424242', color: '#f2f2f2' }}>
+										Discord
+									</MenuItem>
+								</Link>
+							</MenuList>
 						</ButtonGroup>
-						<ButtonGroup display="flex" justifyContent="flex-end" gap={2}>
-							<Button
+						<ButtonGroup display="flex" justifyContent="flex-end" >
+							<IconButton
 								size="sm"
 								color='#f9f9f9'
 								_hover={{ bg: '#424242' }}
 								variant='ghost'>
 								<TbWorld />
-							</Button>
-							<Button size="sm" _hover={{ bg: '#FF5A5A' }} colorScheme='red.900'>PLAY NOW</Button>
+							</IconButton>
+							<Link as={RouterLink} to='https://auth.riotgames.com/login#client_id=prod-xsso-playvalorant&code_challenge=2nBlSKqP0TIhkVNc185OGXrw22wjIViDZm-vjJanyJE&code_challenge_method=S256&redirect_uri=https%3A%2F%2Fxsso.playvalorant.com%2Fredirect&response_type=code&scope=openid%20account&state=f55fc23bc2e96e6991914241d6&uri=https%3A%2F%2Fplayvalorant.com%2Fen-us%2F' style={{ textDecoration: 'none' }}>
+								<Button size="sm" _hover={{ bg: '#ff4655' }} colorScheme='red' color='#111'>PLAY NOW</Button>
+							</Link>
 						</ButtonGroup>
 					</Menu>
 				</Flex>
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,9 +1,10 @@
-import { Box, Center } from "@chakra-ui/react";
 import React from "react";
-import { Card } from "../components/Card";
+import { Link as RouterLink } from "react-router-dom"
 import { Navbar } from "../components/menu/Navbar";
+import { Sidebar } from "../components/sidebar/Sidebar";
 import { Video } from "../components/Video";
 import { useFetch } from "../hooks/useFetch";
+import { Box, Button, Center, Container, Link, Text } from "@chakra-ui/react";
 
 export const Home = () => {
 
@@ -14,10 +15,18 @@ export const Home = () => {
 	return (
 		<>
 			<Navbar />
-			<Box pt='80px'>
-				<Center>
-					<Video />
-
+			<Box pt='80px' position='relative' overflow='hidden' w='100%' fontFamily="FF Mark W05,Arial,sans-serif">
+				<Video />
+				<Container position='absolute' top='30%' left='35%' p={0} >
+					<Center flexDirection='column' >
+						<Text color='whiteAlpha.900'>A 5v5 character-based tactical shooter</Text>
+						<Sidebar />
+					</Center>
+				</Container>
+				<Center position='absolute' top='55%' left='42%' p={0}>
+					<Link as={RouterLink} to='https://playvalorant.com/en-us/download/' style={{ textDecoration: "none" }}>
+						<Button colorScheme='red' color='#ece8e1' _hover={{ transition: 'background-color .2s ease-in', bg: '#0f1923' }} w='300px' h='60px'>PLAY FREE</Button>
+					</Link>
 				</Center>
 			</Box>
 		</>
