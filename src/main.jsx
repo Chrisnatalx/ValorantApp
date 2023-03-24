9a12da5492e14df1c3c9efc0cd998c281e6c6a04
Chrisnatalx <natale.christian.a@gmail.com>
2023-03-24 14:55:44 -0300

    fix bug in main

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,14 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./AppRouter";
 import { ChakraProvider } from "@chakra-ui/react";
-import { ContextProvider } from "./context/ContextProvider";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
 	<React.StrictMode>
-		<ContextProvider>
 		<BrowserRouter>
 			<ChakraProvider>
 				<AppRouter />
 			</ChakraProvider>
 		</BrowserRouter>
-		</ContextProvider>
+
 	</React.StrictMode>
 );
