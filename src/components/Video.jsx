dbb04fbb37165178e451d65c3bc3ab7d6ea00e6e
Chrisnatalx <natale.christian.a@gmail.com>
2024-12-18 12:30:10 -0300

    Change videoHomeView

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
+import React from 'react';
 
 export const Video = () => {
-    return (
-        <video
-            width='100%'
-            preload='true'
-            muted
-            loop
-            playsInline={true}
-            autoPlay={true}
-            poster="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blte299f23c6e55ebed/63bcad4899e03c1edced9b6f/VAL_Ep6_Homepage-CG-Still.jpg"
-            data-testid="hero:video"
-            src="https://assets.contentstack.io/v3/assets/bltb6530b271fddd0b1/blt778f65cedfee54fd/63bcad5b08dfb21202a7794d/VAL_Ep6_Homepage-CG-Video_V5.mp4"
-            type="video/mp4">
-
-        </video>
-    )
-}
+	return (
+		<video
+			width="100%"
+			preload="true"
+			muted
+			loop
+			playsInline={true}
+			autoPlay={true}
+			poster="https://images.contentstack.io/v3/assets/bltb6530b271fddd0b1/blte299f23c6e55ebed/63bcad4899e03c1edced9b6f/VAL_Ep6_Homepage-CG-Still.jpg"
+			data-testid="hero:video"
+			src="https://cmsassets.rgpub.io/sanity/files/dsfx7636/news/409ab2fc369ba5e1fe50bac10c6676d7d1365a9f.mp4"
+			type="video/mp4"
+		></video>
+	);
+};
